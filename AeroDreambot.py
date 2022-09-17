8aea8dd38845650ef6138cf7f931123dfd2aab34
Konstantin Lapitanov <89107827+Konstannttiinn1@users.noreply.github.com>
2022-09-17 21:59:18 +0300

    Update AeroDreambot.py

--- AeroDreambot.py
+++ AeroDreambot.py
@@ -2,12 +2,26 @@ import telebot
 from telebot import types
 
 bot = telebot.TeleBot("")
-markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-item1 = types.KeyboardButton("Запись на полёт")
-item2 = types.KeyboardButton("Что одеть в полёт")
-item3 = types.KeyboardButton("Где находится аэротруба")
-item4 = types.KeyboardButton("Стоимость полёта")
-markup.add(item1, item2, item3, item4)
+
+menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+item1 = types.KeyboardButton("Цены")
+item2 = types.KeyboardButton("Запись на полёт ✈")
+item3 = types.KeyboardButton("Купить подарочный сертификат")
+item4 = types.KeyboardButton("Основные вопросы (faq)")
+item8 = types.KeyboardButton("Как нас найти")
+
+location = types.ReplyKeyboardMarkup(resize_keyboard=True)
+item5 = types.KeyboardButton("Локация")
+item6 = types.KeyboardButton("Фото")
+item7 = types.KeyboardButton("Назад")
+
+inlineprice = types.InlineKeyboardMarkup(row_width=2)
+item10 = types.InlineKeyboardButton("Тарифя для детей", callback_data="Тарифя для детей", url="https://aerodream.spb.ru/tariffs/tarify-dlya-detey/" )
+item11 = types.InlineKeyboardButton("Тарифя для взросылх", callback_data="Тарифя для взросылх", url="https://aerodream.spb.ru/tariffs/tarify-dlya-vzroslykh/")
+inlineprice.add(item10, item11)
+
+location.add(item5, item6, item7)
+menu.add(item1, item2, item3, item4, item8)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -18,33 +32,26 @@ def start_message(message):
 
 @bot.message_handler(commands=['Menu'])
 def button_message(message):
-    bot.send_message(message.chat.id, 'Меню', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Меню', reply_markup=menu)
 
 
-@bot.message_handler(content_types='text')
+@bot.message_handler(content_types=['text'])
 def message_reply(message):
-    if message.text == "Запись на полёт":
+    if message.text == "Запись на полёт ✈":
         bot.send_message(message.chat.id,
-                         " Для записи на полёт можно позвонить по номеру телефона () либо написать нашему администратору ( @79117887506)")
-    elif message.text == "Что одеть в полёт":
-        bot.send_message(message.chat.id, " Одевай обычную одежду ")
-        elif message.text == "Что одеть в полёт":
-        bot.send_message(message.chat.id, " Одевай обычную одежду ")
-    elif message.text == "Где находится аэротруба":
-        markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item5 = types.KeyboardButton("Локация")
-        item6 = types.KeyboardButton("Фото")
-        item7 = types.KeyboardButton("Назад")
-        markup2.add(item5, item6, item7)
-        bot.send_message(message.chat.id, 'Меню', reply_markup=markup2)
+    " Для записи на полёт можно позвонить по номеру телефона () либо написать нашему администратору ( @79117887506)")
+    elif message.text == "Основные вопросы (faq)":
+        bot.send_message(message.chat.id, "Основные вопросы (faq) В РАЗРАБОТКЕ")
+    elif message.text == "Как нас найти":
+        bot.send_message(message.chat.id, 'Меню', reply_markup=location)
     elif message.text == "Локация":
         bot.send_location(message.chat.id, 59.87177, 30.34638)
     elif message.text == "Фото":
         bot.send_photo(message.chat.id, open('C:\\Users\\79215\\PycharmProjects\\pythonProject1\\kap.jpg', 'rb'))
     elif message.text == "Назад":
-        bot.send_message(message.chat.id, 'Меню', reply_markup=markup)
-    elif message.text == "Стоимость полёта":
-        bot.send_message(message.chat.id)
+        bot.send_message(message.chat.id, 'Меню', reply_markup=menu)
+    elif message.text == "Цены":
+        bot.send_message(message.chat.id, "Выбирай", reply_markup=inlineprice)
     else:
         bot.send_message(message.chat.id, " я не понимаю =(")
 
