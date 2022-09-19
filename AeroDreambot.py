0dd7a23b1fda5e4a5e2d5441e6a028d8c9f5bebd
Konstantin Lapitanov <89107827+Konstannttiinn1@users.noreply.github.com>
2022-09-19 20:14:53 +0300

    Update AeroDreambot.py

--- AeroDreambot.py
+++ AeroDreambot.py
@@ -1,27 +1,8 @@
 import telebot
 from telebot import types
+import val
 
-bot = telebot.TeleBot("")
-
-menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
-item1 = types.KeyboardButton("Цены")
-item2 = types.KeyboardButton("Запись на полёт ✈")
-item3 = types.KeyboardButton("Купить подарочный сертификат")
-item4 = types.KeyboardButton("Основные вопросы (faq)")
-item8 = types.KeyboardButton("Как нас найти")
-
-location = types.ReplyKeyboardMarkup(resize_keyboard=True)
-item5 = types.KeyboardButton("Локация")
-item6 = types.KeyboardButton("Фото")
-item7 = types.KeyboardButton("Назад")
-
-inlineprice = types.InlineKeyboardMarkup(row_width=2)
-item10 = types.InlineKeyboardButton("Тарифя для детей", callback_data="Тарифя для детей", url="https://aerodream.spb.ru/tariffs/tarify-dlya-detey/" )
-item11 = types.InlineKeyboardButton("Тарифя для взросылх", callback_data="Тарифя для взросылх", url="https://aerodream.spb.ru/tariffs/tarify-dlya-vzroslykh/")
-inlineprice.add(item10, item11)
-
-location.add(item5, item6, item7)
-menu.add(item1, item2, item3, item4, item8)
+bot = telebot.TeleBot("5302829505:AAHUFUlvEg4q5yUwCkvaqrTRRp9WbhHPcSs")
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -32,7 +13,7 @@ def start_message(message):
 
 @bot.message_handler(commands=['Menu'])
 def button_message(message):
-    bot.send_message(message.chat.id, 'Меню', reply_markup=menu)
+    bot.send_message(message.chat.id, 'Меню', reply_markup=val.menu)
 
 
 @bot.message_handler(content_types=['text'])
@@ -43,15 +24,17 @@ def message_reply(message):
     elif message.text == "Основные вопросы (faq)":
         bot.send_message(message.chat.id, "Основные вопросы (faq) В РАЗРАБОТКЕ")
     elif message.text == "Как нас найти":
-        bot.send_message(message.chat.id, 'Меню', reply_markup=location)
+        bot.send_message(message.chat.id, 'Меню', reply_markup=val.location)
     elif message.text == "Локация":
         bot.send_location(message.chat.id, 59.87177, 30.34638)
     elif message.text == "Фото":
         bot.send_photo(message.chat.id, open('C:\\Users\\79215\\PycharmProjects\\pythonProject1\\kap.jpg', 'rb'))
     elif message.text == "Назад":
-        bot.send_message(message.chat.id, 'Меню', reply_markup=menu)
+        bot.send_message(message.chat.id, 'Меню', reply_markup=val.menu)
     elif message.text == "Цены":
-        bot.send_message(message.chat.id, "Выбирай", reply_markup=inlineprice)
+        bot.send_message(message.chat.id, "Тарифы", reply_markup=val.inlineprice)
+    elif message.text == "Купить подарочный сертификат":
+        bot.send_message(message.chat.id, "купить подарочный сертификат", reply_markup=val.certificates)
     else:
         bot.send_message(message.chat.id, " я не понимаю =(")
 
