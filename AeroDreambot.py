3c2ed291a18fff23d1d91bc0993a8a196d016e3a
Konstannttiinn1 <drambumbes@gmail.com>
2022-10-12 15:24:52 +0300

    add file2

--- AeroDreambot.py
+++ AeroDreambot.py
@@ -2,8 +2,7 @@ import telebot
 from telebot import types
 import val
 
-bot = telebot.TeleBot("5302829505:AAHUFUlvEg4q5yUwCkvaqrTRRp9WbhHPcSs")
-x = 1
+bot = telebot.TeleBot("5046580563:AAF7g_LQHTOoxV4rmnAJG6Dbb4yTmK6ETRM")
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Меню', reply_markup=val.menu)
@@ -32,18 +31,26 @@ def message_reply(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def cal(call):
-    if call.data == "Что одеть?":
-        bot.send_message(call.message.chat.id, "Тест одежда")
-    elif call.data == "время":
-        bot.send_message(call.message.chat.id, "во время")
-    elif call.data == "что с собой брать?":
-        bot.send_message(call.message.chat.id, "настроение")
-    elif call.data == "где можно переодеться?":
-        bot.send_message(call.message.chat.id, "переодется можно у нас")
+    if call.data == "дети":
+        bot.send_message(call.message.chat.id, "Дети летают с 5 лет" "\nДетьми считются до 14 лет включительно"
+                                                "\nДети летают с инструкторами")
+    elif call.data == "полёт":
+        bot.send_message(call.message.chat.id, "Летаем всегда друг за другом, по очереди и с инструктором")
+    elif call.data == "вес":
+        bot.send_message(call.message.chat.id, "Летаем до 120 кг")
+    elif call.data == "билеты":
+        bot.send_message(call.message.chat.id,"Отличие между ними только в оформлении. Электронный билет приходит на почту"
+                         "\nПодарочный сертификат- это симпатичная коробочка, формата А6, внутри инструкция и сам сертификат в виде открытки")
+    elif call.data == "срок":
+        bot.send_message(call.message.chat.id, "Срок действия электронного билета и подарочного сертификата 9 месяцев")
+    elif call.data == "бронь":
+        bot.send_message(call.message.chat.id, "Работаем по предварительной записи со среды по воскресенье полёты проходят С 11 до 19"
+                         "\nЗаписаться можно по номеру телефона +7(800)775-97-11 или написать нашему администратору @aerodream_aerotruba")
     elif call.data == "Тарифы для детей":
         bot.send_message(call.message.chat.id, val.pricechildren)
-    elif call.data == "Тарифы для взрослых":
+    elif call.data == "Тарифы":
         bot.send_message(call.message.chat.id, val.Priceman)
+
 
 
 bot.infinity_polling(timeout=0.5)
