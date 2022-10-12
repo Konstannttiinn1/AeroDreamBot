558039b14882a626af058ebfb0dada0b21c3933c
Konstantin Lapitanov <89107827+Konstannttiinn1@users.noreply.github.com>
2022-10-12 15:29:11 +0300

    Update val.py

--- val.py
+++ val.py
@@ -2,13 +2,14 @@ import telebot
 from telebot import types
 
 questions = types.InlineKeyboardMarkup(row_width=2)
-questions1 = types.InlineKeyboardButton("Что одеть?", callback_data="Что одеть?")
-questions2 = types.InlineKeyboardButton("в какое время приходить?", callback_data="время")
-questions3 = types.InlineKeyboardButton("что с собой брать?", callback_data="что с собой брать?")
-questions4 = types.InlineKeyboardButton("где можно переодеться?", callback_data="где можно переодеться?")
-questions5 = types.InlineKeyboardButton("Со скольки лет полёт?", callback_data="Со скольки лет полёт?")
+questions1 = types.InlineKeyboardButton("Вопросы про детей", callback_data="дети")
+questions2 = types.InlineKeyboardButton("Как проходит полёт", callback_data="полёт")
+questions3 = types.InlineKeyboardButton("Ограничения по весу", callback_data="вес")
+questions4 = types.InlineKeyboardButton("Билет и сертификат", callback_data="билеты")
+questions5 = types.InlineKeyboardButton("Срок действия билетов", callback_data="срок")
+questions6 = types.InlineKeyboardButton("Бронирование полёта", callback_data="бронь")
 
-questions.add(questions4, questions3, questions2, questions1, questions5)
+questions.add(questions4, questions5, questions2, questions1, questions3, questions6)
 
 certificates = types.InlineKeyboardMarkup(row_width=1)
 item12 = types.InlineKeyboardButton("Ссылка на сайт", url="https://aerodream.spb.ru/certificates/" )
@@ -16,7 +17,7 @@ certificates.add(item12)
 
 inlineprice = types.InlineKeyboardMarkup(row_width=2)
 price1 = types.InlineKeyboardButton("Тарифы для детей", callback_data="Тарифы для детей", cache_time=False)
-price2 = types.InlineKeyboardButton("Тарифы для взросылх", callback_data="Тарифы для взрослых")
+price2 = types.InlineKeyboardButton("Тарифы", callback_data="Тарифы")
 inlineprice.add(price2, price1)
 
 menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -35,10 +36,5 @@ location.add(location1, location2, location3)
 
 recordfly = "Для записи на полёт можно позвонить по номеру телефона +7(800)775-97-11  либо написать нашему администратору (@aerodream_aerotruba) \nЗаписаться на полёт или задать вопросы можно с 10 до 22"
 
-Priceman = ("Старт - 3 минуты = 3290 руб.\nВзлёт - 5 минут = 4000 руб.\nНовичок х2 - 10 минут = 6000 руб.\nСемейный - 15 минут = 8800 руб.\nДрайв - 20 минут = 10290 руб.\nПраздник в аэротрубе - 60 минут = 24000 руб."
-         "\n"
-         "\nдля более подробной информацией и посмотреть акции, можно пройти по ссылке")
-pricechildren = ("Старт детский - 3 минуты = 2090 руб.\nВзлёт детский - 5 минут = 3090 руб.\nНебо детский - 10 минут = 5290 руб.\nДетский праздник - 60 минут = 19990 руб."
-         "\n"
-         "\nдля более подробной информацией и посмотреть акции, можно пройти по ссылке")
-
+Priceman = ("Старт - 3 минуты = 3290 руб.\nВзлёт - 5 минут = 4000 руб.\nНовичок х2 - 10 минут = 6000 руб.\nСемейный - 15 минут = 8800 руб.\nДрайв - 20 минут = 10290 руб.\nПраздник в аэротрубе - 60 минут = 24000 руб.")
+pricechildren = ("Старт детский - 3 минуты = 2090 руб.\nВзлёт детский - 5 минут = 3090 руб.\nНебо детский - 10 минут = 5290 руб.\nДетский праздник - 60 минут = 19990 руб.")
